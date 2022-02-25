95ea4143de2971b553536a1de9146c717bdb14cf
Documenter.jl <documenter@juliadocs.github.io>
2022-02-25 20:19:04 +0000

    build based on 6939e6a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleNeRF","category":"page"},{"location":"#SimpleNeRF","page":"Home","title":"SimpleNeRF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleNeRF.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleNeRF]","category":"page"}]
+}
